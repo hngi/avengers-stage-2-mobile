e486aae218ea8328a9e9358119f487708a52470e
Jimoh Babatunde <jimoh.abdullahi@konga.com>
2020-06-11 12:25:57 +0100

    Added app icon and launch screen for ios

---
+++ brainy/build/ios/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/GeneratedPluginRegistrant.d
@@ -0,0 +1,19 @@
+dependencies: \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Runner/GeneratedPluginRegistrant.m \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Runner/GeneratedPluginRegistrant.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Modules/module.modulemap \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterTexture.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterPlugin.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterEngine.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterDartProject.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterBinaryMessenger.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterViewController.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterPluginAppLifeCycleDelegate.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterPlatformViews.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterHeadlessDartRunner.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterChannels.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterCallbackCache.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterCodecs.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterMacros.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/FlutterAppDelegate.h \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/ios/Flutter/Flutter.framework/Headers/Flutter.h
---
+++ brainy/build/ios/Runner.build/Debug-iphonesimulator/Runner.build/Objects-normal/x86_64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jimohbabatunde/Desktop/avengers_fork_repo/avengers-stage-2-mobile/brainy/build/ios/Runner.build/Debug-iphonesimulator/Runner.build/DerivedSources/Runner_vers.c
